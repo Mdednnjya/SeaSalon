dffb466ffff0057fb2092924e1c542336da86f80
Danan <narayanmade578@gmail.com>
2024-06-30 14:08:43 +0700

    refactor(assets): consolidate and optimize CSS and JS management via Vite, close #7

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,21 +7,14 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>SEA-salon</title>
         <link rel="icon" href="{{ asset('images/logo.svg') }}" type="image/icon" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link href='https://fonts.googleapis.com/css?family=Encode Sans Semi Expanded' rel='stylesheet'>
-        <link href="https://fonts.googleapis.com/css2?family=Encode+Sans+Semi+Expanded:wght@100;200;300;400;500;600;700;800;900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         @include('components.header')
-
         <div class="container">
             @yield('content')
         </div>
-
         @include('components.footer')
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
